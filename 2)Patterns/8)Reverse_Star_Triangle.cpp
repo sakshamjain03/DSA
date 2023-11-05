54e49062141858071e1a4103f50b32dbbc2a6bf4
sakshamjain03 <sakjay13@gmail.com>
2023-11-06 01:30:37 +0530

    Git ignore edited

--- 2)Patterns/8)Reverse_Star_Triangle.cpp
+++ 2)Patterns/8)Reverse_Star_Triangle.cpp
@@ -12,28 +12,37 @@
 // Sample Input 2 :
 // 1
 // Sample Output 2 :
-// *
 #include <iostream>
 using namespace std;
 
+// void nStarTriangle(int n) {
+//     for(int i=n;i>0;i--){
+//         for(int a=i;a<n;a++){
+//             cout<<" ";
+//         }
+//         for(int j=0;j<i;j++){
+//         cout<<"*";
+//         }
+//         for(int j=1;j<i;j++){
+//         cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
 void nStarTriangle(int n) {
-    for(int i=n;i>0;i--){
-        for(int a=i;a<n;a++){
+    for(int i=0;i<n;i++){
+        int gap=0, star=2*n-1;
+        for(int a=0;a<gap;a++){
             cout<<" ";
         }
-        for(int j=0;j<i;j++){
+        for(int j=gap;j<gap+star;j++){
         cout<<"*";
-        }
-        for(int j=1;j<i;j++){
-        cout<<"*";
-        }
-        for(int a=i;a<n;a++){
-            cout<<" ";
         }
         cout<<endl;
+        gap++;
+        star-=2;
     }
 }
-
 
 int main() {
     int n;
