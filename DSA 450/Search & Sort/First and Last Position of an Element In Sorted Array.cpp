d8401d0efb04a55d9e5bc4999995fa1993837d9b
Saksham Jain <112430888+sakshamjain03@users.noreply.github.com>
2024-02-20 10:00:10 +0530

    date commit

---
+++ DSA 450/Search & Sort/First and Last Position of an Element In Sorted Array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    int first=-1,last=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==k){
+            if(first==-1){
+                first=i;
+                last=i;
+            }
+            else{
+                last=i;
+            }
+        }
+    }
+    return make_pair(first,last);
+}
